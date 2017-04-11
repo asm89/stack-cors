f559d1393c7574139442f03f4c8c55b1a7ce854f
Barry vd. Heuvel <barryvdh@gmail.com>
2017-04-11 22:15:31 +0200

    Update copyright

--- test/Asm89/Stack/OriginMatcherTest.php
+++ test/Asm89/Stack/OriginMatcherTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of asm89/stack-cors.
+ *
+ * (c) Alexander <iam.asm89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Asm89\Stack;
 
 use PHPUnit_Framework_TestCase;
