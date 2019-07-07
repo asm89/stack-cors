612798cbfac784c89b3df41a578852d224779104
pietrolerro <38533139+pietrolerro@users.noreply.github.com>
2019-07-07 02:45:06 +0200

    Update CorsTest.php
    updated tests

--- test/Asm89/Stack/CorsTest.php
+++ test/Asm89/Stack/CorsTest.php
@@ -103,7 +103,7 @@ class CorsTest extends PHPUnit_Framework_TestCase
 
         $response = $app->handle($request);
 
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(204, $response->getStatusCode());
         $this->assertEquals('FOO, BAR', $response->headers->get('Access-Control-Allow-Headers'));
     }
 
@@ -354,7 +354,7 @@ class CorsTest extends PHPUnit_Framework_TestCase
 
         $response = $app->handle($request);
 
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(204, $response->getStatusCode());
 
         $this->assertTrue($response->headers->has('Access-Control-Allow-Headers'));
         // the response will have the "allowedHeaders" value passed to Cors rather than the request one
@@ -412,7 +412,7 @@ class CorsTest extends PHPUnit_Framework_TestCase
         $request->headers->set('Access-Control-Request-Headers', '');
         
         $response = $app->handle($request);
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(204, $response->getStatusCode());
     }
 
     private function createValidActualRequest()
