8b27af9955b5b587da2eab7f3c0dac3c5352a428
Barry vd. Heuvel <barry@fruitcake.nl>
2022-01-03 16:44:31 +0100

    Only support Symfony 6

--- tests/MockApp.php
+++ tests/MockApp.php
@@ -16,7 +16,7 @@ class MockApp implements HttpKernelInterface
         $this->responseHeaders = $responseHeaders;
     }
 
-    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true): Response
     {
         $response = new Response();
 
