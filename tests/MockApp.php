1a090b83edfac984353625bb5bb902b2853a282e
Barry vd. Heuvel <barry@fruitcake.nl>
2022-01-03 16:33:55 +0100

    Test 2 versions

--- tests/MockApp.php
+++ tests/MockApp.php
@@ -6,22 +6,46 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-class MockApp implements HttpKernelInterface
-{
-
-    private $responseHeaders;
-
-    public function __construct(array $responseHeaders)
+if (!defined(HttpKernelInterface::MAIN_REQUEST)) {
+    class MockApp implements HttpKernelInterface
     {
-        $this->responseHeaders = $responseHeaders;
+
+        private $responseHeaders;
+
+        public function __construct(array $responseHeaders)
+        {
+            $this->responseHeaders = $responseHeaders;
+        }
+
+        public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+        {
+            $response = new Response();
+
+            $response->headers->add($this->responseHeaders);
+
+            return $response;
+        }
     }
 
-    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+} else {
+    class MockApp implements HttpKernelInterface
     {
-        $response = new Response();
 
-        $response->headers->add($this->responseHeaders);
+        private $responseHeaders;
 
-        return $response;
+        public function __construct(array $responseHeaders)
+        {
+            $this->responseHeaders = $responseHeaders;
+        }
+
+        public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true) : Response
+        {
+            $response = new Response();
+
+            $response->headers->add($this->responseHeaders);
+
+            return $response;
+        }
     }
+
 }
