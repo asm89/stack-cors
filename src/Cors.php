7b3927a74adff55bf91bf968e2d908de079f9343
Barry vd. Heuvel <barry@fruitcake.nl>
2022-01-18 10:21:16 +0100

    Revert "Add returntype"
    This reverts commit b0850815f56500a231a107668ef0a8518f0de882.

--- src/Cors.php
+++ src/Cors.php
@@ -11,7 +11,6 @@
 
 namespace Asm89\Stack;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -43,7 +42,7 @@ class Cors implements HttpKernelInterface
         $this->cors = new CorsService(array_merge($this->defaultOptions, $options));
     }
 
-    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true): Response
+    public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
     {
         if ($this->cors->isPreflightRequest($request)) {
             $response = $this->cors->handlePreflightRequest($request);
